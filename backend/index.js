a7a5640a69b3075f6b0b283e1b398dfa3c4ce3bf
Pushkkaarr <pushkarmmhatre3090@gmail.com>
2025-11-30 21:47:39 +0530

    fix : render calling enabled to prevent suspension on server

--- backend/index.js
+++ backend/index.js
@@ -31,27 +31,24 @@ app.get('/api/health', (req, res) => res.json({ ok: true }));
 app.use('/api/events', eventRoutes);
 app.use('/api/auth', authRoutes);
 
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+
+    // Keep-alive ping to prevent Render app from sleeping
+    const url = process.env.B_LINK; // Your Render app URL
+    const interval = 13 * 60 * 1000; // 13 minutes
+
+    // Keep-alive ping to prevent Render app from sleeping
+    if (url) {
+        setInterval(() => {
+            axios.get(url)
+                .then(response => {
+                    console.log(`Keep-alive ping at ${new Date().toISOString()}: Status ${response.status}`);
+                })
+                .catch(error => {
+                    console.error(`Keep-alive ping error at ${new Date().toISOString()}:`, error.message);
+                });
+        }, interval);
+    }
 });
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-
-//     // Keep-alive ping to prevent Render app from sleeping
-//     const url = process.env.B_LINK; // Your Render app URL
-//     const interval = 13 * 60 * 1000; // 13 minutes
-
-//     // Keep-alive ping to prevent Render app from sleeping
-//     if (url) {
-//         setInterval(() => {
-//             axios.get(url)
-//                 .then(response => {
-//                     console.log(`Keep-alive ping at ${new Date().toISOString()}: Status ${response.status}`);
-//                 })
-//                 .catch(error => {
-//                     console.error(`Keep-alive ping error at ${new Date().toISOString()}:`, error.message);
-//                 });
-//         }, interval);
-//     }
-// });
